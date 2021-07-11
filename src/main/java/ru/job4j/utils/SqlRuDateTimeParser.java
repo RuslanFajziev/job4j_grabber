566abed4295ff4699bc34e65a3215486fef2c620
FaizievRT <rus23f@mail.ru>
2021-07-11 17:54:53 +0300

    2.1. Преобразование даты [#289476]

---
+++ src/main/java/ru/job4j/utils/SqlRuDateTimeParser.java
@@ -0,0 +1,46 @@
+package ru.job4j.utils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SqlRuDateTimeParser implements DateTimeParser {
+    @Override
+    public LocalDateTime parse(String parse) {
+        Map<String, String> monthLocal = new HashMap<>();
+        monthLocal.put("янв", "01");
+        monthLocal.put("фев", "02");
+        monthLocal.put("мар", "03");
+        monthLocal.put("апр", "04");
+        monthLocal.put("май", "05");
+        monthLocal.put("июн", "06");
+        monthLocal.put("июл", "07");
+        monthLocal.put("авг", "08");
+        monthLocal.put("сен", "09");
+        monthLocal.put("окт", "10");
+        monthLocal.put("ноя", "11");
+        monthLocal.put("дек", "12");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        String dataLocal = "0001-01-01";
+        String[] arrayDate = parse.split(" ");
+        if (arrayDate.length == 2) {
+            if (arrayDate[0].equals("вчера,")) {
+                dataLocal = LocalDate.now().minusDays(1).toString();
+            } else if (arrayDate[0].equals("сегодня,")) {
+                dataLocal = LocalDate.now().toString();
+            }
+            return LocalDateTime.parse(dataLocal + " " + arrayDate[1], formatter);
+        } else if (arrayDate.length == 4) {
+            String year = arrayDate[2];
+            year = year.substring(0, year.length() - 1); // убрали запятую в конце года
+            year = year.length() == 2 ? "20" + year : year; // делаем полную версию года
+            String date = arrayDate[0];
+            date = date.length() == 1 ? "0" + date : date; // делаем полную версию даты
+            String month = monthLocal.get(arrayDate[1]); // заменяем короткое название месяца на номер месяца
+            return LocalDateTime.parse(year + "-" + month + "-" + date + " " + arrayDate[3], formatter);
+        }
+        return null;
+    }
+}
---
+++ src/test/java/ru/job4j/utils/SqlRuDateTimeParserTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.utils;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class SqlRuDateTimeParserTest {
+
+    @Test
+    public void test1() {
+        SqlRuDateTimeParser sqlRuDateTimeParser = new SqlRuDateTimeParser();
+        LocalDateTime localDateTime = sqlRuDateTimeParser.parse("2 июл 21, 10:51");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime checkLocalDateTime = LocalDateTime.parse("2021-07-02 10:51", formatter);
+        assertThat(localDateTime, is(checkLocalDateTime));
+    }
+
+    @Test
+    public void test2() {
+        SqlRuDateTimeParser sqlRuDateTimeParser = new SqlRuDateTimeParser();
+        LocalDateTime localDateTime = sqlRuDateTimeParser.parse("вчера, 10:51");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime checkLocalDateTime = LocalDateTime.parse(LocalDate.now().minusDays(1) + " 10:51", formatter);
+        assertThat(localDateTime, is(checkLocalDateTime));
+    }
+
+    @Test
+    public void test3() {
+        SqlRuDateTimeParser sqlRuDateTimeParser = new SqlRuDateTimeParser();
+        LocalDateTime localDateTime = sqlRuDateTimeParser.parse("сегодня, 23:45");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime checkLocalDateTime = LocalDateTime.parse(LocalDate.now() + " 23:45", formatter);
+        assertThat(localDateTime, is(checkLocalDateTime));
+    }
+
+    @Test
+    public void test4() {
+        SqlRuDateTimeParser sqlRuDateTimeParser = new SqlRuDateTimeParser();
+        LocalDateTime localDateTime = sqlRuDateTimeParser.parse("11 июл 2021, 10:45");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime checkLocalDateTime = LocalDateTime.parse("2021-07-11 10:45", formatter);
+        assertThat(localDateTime, is(checkLocalDateTime));
+    }
+
+    @Test
+    public void test5() {
+        SqlRuDateTimeParser sqlRuDateTimeParser = new SqlRuDateTimeParser();
+        LocalDateTime localDateTime = sqlRuDateTimeParser.parse("11 июл 20 21, 10:45");
+        assertNull(localDateTime);
+    }
+}
