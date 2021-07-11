79d8ed836b19bc96f2a35dc4262e1b231eda52f0
FaizievRT <rus23f@mail.ru>
2021-07-11 19:38:43 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,23 +5,38 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
+
 public class SqlRuParse {
-    public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+    private static int numberRows = 0;
+
+    public static void parse(String url) throws IOException {
+        Document doc = Jsoup.connect(url).get();
         Elements row = doc.getElementsByClass("ForumTable").get(0).getElementsByTag("tr");
         for (Element td : row) {
             if (td.text().contains("Тема Автор Ответов Просм. Дата")) {
-                System.out.println("-------------------------------------------------------------------------------------");
-                System.out.println("Ссылка                                           Тема                            Дата");
-                System.out.println("-------------------------------------------------------------------------------------");
                 continue;
             }
             Elements nm = td.getElementsByTag("tr").get(0).getElementsByTag("td");
-            String ellink = nm.get(1).getElementsByTag("a").get(0).attr("href");
+            String elLink = nm.get(1).getElementsByTag("a").get(0).attr("href");
             String elSubject = nm.get(1).getElementsByTag("a").get(0).text();
             String elDate = nm.get(5).text();
-            System.out.printf("%s - \"%s\" - (%s)\n", ellink, elSubject, elDate);
+            System.out.printf("%s - \"%s\" - (%s)\n", elLink, elSubject, elDate);
+            numberRows += 1;
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+        String baseUrl = "https://www.sql.ru/forum/job-offers";
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("Ссылка                                           Тема                            Дата");
+        System.out.println("-------------------------------------------------------------------------------------");
+        for (int index = 1; index <= 5; index++) {
+            String queryUrl = baseUrl + "/" + index;
+            parse(queryUrl);
+        }
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.printf("           Number of rows: %s%n", numberRows);
         System.out.println("-------------------------------------------------------------------------------------");
     }
 }
