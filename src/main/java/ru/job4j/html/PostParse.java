434cdfe7a19b45d6323dca7d4d66ef8127b1c0fe
FaizievRT <rus23f@mail.ru>
2021-07-12 23:54:46 +0300

    2.3. Загрузка деталей поста. [#285212 #176572] v2

--- src/main/java/ru/job4j/html/PostParse.java
+++ src/main/java/ru/job4j/html/PostParse.java
@@ -2,26 +2,15 @@ package ru.job4j.html;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
 import java.io.IOException;
 
 public class PostParse {
     public static void parse(String url) throws IOException {
         Document doc = Jsoup.connect(url).get();
-        Elements row = doc.getElementsByClass("msgTable").get(0).getElementsByTag("tr");
-        for (Element td : row) {
-            if (td.text().contains("Москва, до 200т")) {
-                continue;
-            } else if (td.text().contains("Цитировать Сообщить модератору")) {
-                String data = td.text().substring(1, 16);
-                System.out.println(data);
-                continue;
-            }
-            String mess = td.getElementsByTag("tr").get(0).getElementsByTag("td").get(1).text();
-            System.out.println(mess);
-        }
+        String msbBody = doc.getElementsByClass("msgBody").get(1).text();
+        String msbFooter = doc.getElementsByClass("msgFooter").get(0).text().substring(1, 16);
+        System.out.println(msbBody);
+        System.out.println(msbFooter);
     }
 
     public static void main(String[] args) throws Exception {
