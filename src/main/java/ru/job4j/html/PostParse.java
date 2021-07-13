96f80b07ce14a67536dd5adc9f5bc36e77b9f048
FaizievRT <rus23f@mail.ru>
2021-07-13 23:51:59 +0300

    light commit My

--- src/main/java/ru/job4j/html/PostParse.java
+++ src/main/java/ru/job4j/html/PostParse.java
@@ -8,13 +8,17 @@ public class PostParse {
     public static void parse(String url) throws IOException {
         Document doc = Jsoup.connect(url).get();
         String msbBody = doc.getElementsByClass("msgBody").get(1).text();
-        String msbFooter = doc.getElementsByClass("msgFooter").get(0).text().substring(1, 16);
+        String msbFooter = doc.getElementsByClass("msgFooter").get(0).text().substring(0, 15);
         System.out.println(msbBody);
         System.out.println(msbFooter);
+        String msbHeader = doc.getElementsByClass("messageHeader").get(0).text();
+        System.out.println(msbHeader.substring(0, msbHeader.length() - 6));
     }
 
     public static void main(String[] args) throws Exception {
-        String baseUrl = "https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t";
+        String baseUrl = "https://www.sql.ru/forum/1337426/arhitektor-cifrovyh-modeley-ofis-zp-200-300k";
+        parse(baseUrl);
+        baseUrl = "https://www.sql.ru/forum/448729/priglashaem-sistemnogo-administratora-v-p-tomilino";
         parse(baseUrl);
     }
 }
