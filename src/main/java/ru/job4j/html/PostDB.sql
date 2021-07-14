79fba4194c33f6a20b2aa59dddcaca6a21f9a273
FaizievRT <rus23f@mail.ru>
2021-07-14 23:41:15 +0300

    4. Схема таблицы Post [#1731]

--- src/main/java/ru/job4j/html/PostDB.sql
+++ src/main/java/ru/job4j/html/PostDB.sql
@@ -1,9 +1,15 @@
-CREATE DATABASE post_jobs WITH OWNER = postgres ENCODING = 'UTF8' CONNECTION LIMIT = -1;
+CREATE DATABASE jobs WITH OWNER = postgres ENCODING = 'UTF8' CONNECTION LIMIT = -1;
 
 create table post(
-    id int,
-    title varchar(255),
-    link varchar(255),
-    description varchar(255),
+    id serial primary key,
+    name varchar(255),
+    link varchar(255) UNIQUE,
+    text text,
     created timestamp
 );
+
+--ALTER TABLE post ADD UNIQUE (link);
+--
+--select column_name, column_default, data_type, colum_type from information_schema.columns
+--where table_catalog = 'jobs' and
+--table_name = 'post';
